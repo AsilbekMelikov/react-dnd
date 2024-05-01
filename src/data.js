ea47d3ac520e765d503823ec125438e6d58f2d76
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-01 11:33:50 +0500

    Create data

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    id: 1,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 2,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 3,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 4,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+];
+
+const statuses = [
+  {
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46",
+  },
+  {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0",
+  },
+  {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0",
+  },
+  {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE",
+  },
+];
+
+export { data, statuses };
