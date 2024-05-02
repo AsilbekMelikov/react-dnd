2322ea60177adf56be4b81e47718b02082399f47
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-02 17:21:23 +0500

    Create data

---
+++ src/data/data.js
@@ -0,0 +1,83 @@
+const data = [
+  {
+    id: 1,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 2,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 3,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 4,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 5,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 6,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 7,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+  {
+    id: 8,
+    icon: "⭕️",
+    status: "open",
+    title: "Human Interest Form",
+    content: "Fill out human interest distribution form",
+  },
+];
+
+const statususes = [
+  {
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46",
+  },
+  {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0",
+  },
+  {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0",
+  },
+  {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE",
+  },
+];
+
+export { data, statususes };
